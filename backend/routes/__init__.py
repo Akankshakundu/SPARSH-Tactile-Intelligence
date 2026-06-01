64889050e6fcf6678852e7023be5f573474d6cec
its-saksham03 <sakshamjha.2907@gmail.com>
2026-06-02 01:02:05 +0530

    Final Braille Vision submission

--- backend/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from routes.health import router as health_router
-from routes.upload import router as upload_router
-from routes.stream import router as stream_router
-
-__all__ = ["health_router", "upload_router", "stream_router"]
